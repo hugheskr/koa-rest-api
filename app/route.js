ca122f08879f85b12d0356564a17ea4963861a94
SamHeutmaker <samheutmaker@gmail.com>
2016-01-23 12:52:57 -0800

    Package.json updated

--- app/route.js
+++ app/route.js
@@ -1,2 +1,17 @@
-const route = require('koa-route');
+const router = require('koa-route');
 
+
+router.get('/', function*(){
+
+})
+
+
+var middleware = function*(){
+
+	//1 yeild 5;
+	//2 yeild {}
+	//3 yeild []
+}
+
+var first = middleware.next();
+var first = middleware.next();
--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const app = koa();
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:3000/db');
+
+const movieRouter = require(__dirname + '/app/route');
+
+app.use(movieRouter);
+
+app.listen(PORT, () => {
+	console.log('Server stared on port ' + PORT);
+});
+
 
