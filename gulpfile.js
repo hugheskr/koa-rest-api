83f435b17112d53ec90ec2119ab3aafe93c31acd
jamesvermillion <jamesvermillion@yahoo.com>
2016-01-23 12:26:10 -0800

    gulfile fleshed out test updated on package

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 
-gulp.task('test', () => {
+var files = ['index.js', 'gulpfile.js', './lib/*.js', './test/*.js',
+ '!node_modules/**', '!*.json'];
 
-})
+gulp.task('lint', () => {
+  return gulp.src(files)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
 
+gulp.task('mocha', () => {
+  return gulp.src('test/*.js')
+    .pipe(mocha({ reporter: 'nyan' }));
+});
+
+gulp.task('watch', () => {
+  return gulp.watch(files, ['lint', 'mocha']);
+});
+
+gulp.task('default', ['watch', 'lint', 'mocha']);
